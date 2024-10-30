821ba57e61b76680f9ced16e3bb3ea703caaf187
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-10-30 14:01:37 +0530

    done final yayyy

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 export const About = () => {
     return (
         <div className="text-center p-0 m-0 ">
-            <h1 className="gradient-text-heading">JOBLINKER</h1>
+            <h1 className="gradient-text-heading mt-6 mb-12">JOBLINKER</h1>
             {/* <p className="gradient-text">Get the latest links to jobs based on your resume and preferences</p> */}
         </div>
     )
